6a1764492a433186fbd7a43df63e55d5cf17d39a
Natali Kugelman <naftali.kugelman@gmail.com>
2025-08-11 16:49:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const path=require('path');
+
+require("dotenv").config();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'scripts')));
+app.get("/", (req,res)=>{
+    res.render("index")
+})
+
+app.listen(process.env.PORT, (err)=>{
+    if(err)console.log("error" + err.message)
+    else console.log("listening on port" + process.env.PORT )
+})
+
+
